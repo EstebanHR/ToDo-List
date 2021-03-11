5b5fb1850eec5ab1b4acae4c7bb8c98e9fdc517f
EstebanHR <estebanhr1981@hotmail.com>
2021-03-11 02:25:07 +0000

    ToDo List

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	const [task, setTask] = useState("");
+	const [check, setCheck] = useState(false);
+
+	const [list, setList] = useState([]);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5 text-center">
+			<div className="row d-flex justify-content-center">
+				<div className="col-md-10">
+					<div className="card">
+						<h1 className="display-5 text-primary">To Do List</h1>
+						<div className="card-header">
+							<div className="row mt-4">
+								<div className="col-sm-8 pb-3">
+									<label>Task</label>
+									<input
+										className="form-control"
+										type="text"
+										value={task}
+										onChange={e => {
+											setTask(
+												e.target.value.toUpperCase()
+											);
+										}}
+									/>
+								</div>
+								<div className="col-sm-1 pb-3">
+									<label>Done</label>
+									<input
+										type="checkbox"
+										className="form-control"
+										checked={check}
+										onChange={e =>
+											setCheck(e.target.checked)
+										}
+									/>
+								</div>
+								<div className="col-sm-3 pb-3 d-flex align-items-end">
+									<button
+										type="button"
+										className="form-control btn btn-primary"
+										onClick={() => {
+											let obj = {
+												label: task,
+												done: check
+											};
+											setList(list.concat(obj));
+											setCheck(false);
+											setTask("");
+											//console.log({ list });
+										}}>
+										Add List
+									</button>
+								</div>
+							</div>
+						</div>
+						<div className="card-body text-primary">
+							{!list
+								? "loading..."
+								: list.map((item, index) => {
+										return (
+											<label
+												className="form-control text-left px-5"
+												key={index}>
+												<input
+													type="checkbox"
+													checked={item.done}
+												/>
+												{item.label}
+											</label>
+										);
+								  })}
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
